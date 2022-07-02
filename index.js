61dfb3f0232aa4f6715028458a1f060f5b7c5f93
M2K Developments <m2ksoftiez@gmail.com>
2022-07-02 20:29:14 +0200

    opening and closing songs

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
-const officegen = require('officegen')
-const fs = require('fs')
-const path = require('path');
-const PPTX = require('nodejs-pptx');
+const powerpoint = require("./powerpoint");
 
 //Middleware
 app.use(morgan('dev'));
@@ -17,91 +14,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-    let pptx = new PPTX.Composer();
-
-    await pptx.compose(pres => {
-        pres.addSlide(slide => {
-            slide.addText(text => {
-                text.value('Hello World');
-            });
-        });
-    });
-
-    await pptx.save(`./hello-world.pptx`);
-    return res.status(200).json({ re: true });
-});
-
-
-app.get('/download', async (req, res) => {
-    try {
-
-        // Create an empty PowerPoint object:
-        let pptx = officegen('pptx')
-        pptx.setDocTitle('Kabula Hill Bulletin')
-
-        // Let's add a title slide:
-
-        let slide = pptx.makeTitleSlide('Officegen', 'Example to a PowerPoint document');
-        pptx.makeTitleSlide('Officegen', 'Example to a PowerPoint document')
-        pptx.makeTitleSlide('Officegen', 'Example to a PowerPoint document')
-
-        // Pie chart slide example:
-
-        // slide = pptx.makeNewSlide({ userLayout: 'title' });
-        slide.addText('Full line', 0, 40, '100%', 20);
-        slide.name = 'Pie Chart slide'
-        slide.back = 'ffff00'
-
-        // slide.addChart(
-        //     {
-        //         title: 'My production',
-        //         renderType: 'pie',
-        //         data:
-        //             [
-        //                 {
-        //                     name: 'Oil',
-        //                     labels: ['Czech Republic', 'Ireland', 'Germany', 'Australia', 'Austria', 'UK', 'Belgium'],
-        //                     values: [301, 201, 165, 139, 128, 99, 60],
-        //                     colors: ['ff0000', '00ff00', '0000ff', 'ffff00', 'ff00ff', '00ffff', '000000']
-        //                 }
-        //             ]
-        //     }
-        // )
-
-        // Let's generate the PowerPoint document directly into the response stream:
-        res.writeHead(200, {
-            'Content-Type':
-                'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-            'Content-disposition': 'attachment filename=out.pptx'
-        });
-
-        let out = fs.createWriteStream('example.pptx');
-
-
-        // This one catch only the officegen errors:
-        pptx.on('error', function (err) {
-            res.end(err)
-        })
-
-        // Catch response errors:
-        out.on('error', function (err) {
-            res.end(err)
-        })
-
-        // End event after sending the PowerPoint data:
-        out.on('finish', function () {
-            res.end()
-        })
-
-        // This async method is working like a pipe - it'll generate the pptx data and pass it directly into the output stream:
-        pptx.generate(out)
-
-    } catch (e) {
-        console.log(e);
-        return res.status(500).json({ result: false });
-    }
-});
+app.get('/', powerpoint.run);
 
 const port = process.env.PORT || 4000;
 
---
+++ powerpoint.js
@@ -0,0 +1,165 @@
+const PPTX = require('nodejs-pptx');
+const path = require('path');
+
+function getFont(line) {
+    const x = line.length;
+    const m = -1.228571428571429;
+    const c = 151.1428571428572;
+    const y = (x * m) + c;
+    const font = y;
+    return parseInt(font);
+}
+
+function printSong(typeOfSong, pres, number, courtworship = false) {
+
+    const hymn = require(`./res/${number}.json`);
+    const { title, verses, chorus } = hymn;
+    const courtworshipfonts = [110, 110, 100, 100];
+
+    pres.addSlide(slide => {
+
+        //add title
+        slide.addText(text => {
+            text.value(typeOfSong)
+                .x(0)
+                .y(100)
+                .cx(720)
+                .cy(100)
+                .autoFit(true)
+                .fontFace('Calibri')
+                .fontSize(105)
+                // .textColor('CC0000')
+                // .textWrap('none')
+                .textAlign('center')
+                .textVerticalAlign('center')
+                // .line({ color: '0000FF', dashType: 'dash', width: 1.0 })
+                .margin(0);
+        });
+
+
+        //add sub title
+        slide.addText(text => {
+            text.value(`#${title}`)
+                .x(20)
+                .y(230)
+                .cx(680)
+                .cy(100)
+                .fontFace('Calibri')
+                .fontSize(50)
+                // .textColor('CC0000')
+                // .textWrap('none')
+                .textAlign('center')
+                .textVerticalAlign('center')
+                // .line({ color: '0000FF', dashType: 'dash', width: 1.0 })
+                .margin(0);
+        });
+    });
+
+    verses.forEach((verse, index) => {
+        const lines = verse.split('\n');
+        lines.forEach((line, linecount) => {
+            pres.addSlide(slide => {
+                //add verse number
+                slide.addText(text => {
+                    text.value(`Verse ${index + 1}`)
+                        .x(10)
+                        .y(0)
+                        .cx(700)
+                        .cy(30)
+                        .autoFit(true)
+                        .fontFace('Calibri')
+                        .fontSize(20)
+                        .textWrap('none')
+                        .textAlign('left')
+                        .textVerticalAlign('center')
+                        .margin(0);
+                });
+
+
+                //add line
+                slide.addText(text => {
+                    const font = courtworship ? courtworshipfonts[linecount] : getFont(line);
+                    text.value(line.replace(',', '\n'))
+                        .x(0)
+                        .y(10)
+                        .cx(720)
+                        .cy(400)
+                        .autoFit(true)
+                        .fontFace('Calibri')
+                        .fontSize(font)
+                        // .textWrap('none')
+                        .textAlign('center')
+                        .fontBold(true)
+                        .textVerticalAlign('center')
+                        .margin(0);
+                });
+
+                if (chorus !== "") {
+                    const chorus_lines = chorus.split('\n');
+                    chorus_lines.forEach(line => {
+                        pres.addSlide(slide => {
+                            //add verse number
+                            slide.addText(text => {
+                                text.value(`Corus`)
+                                    .x(10)
+                                    .y(0)
+                                    .cx(700)
+                                    .cy(30)
+                                    .autoFit(true)
+                                    .fontFace('Calibri')
+                                    .fontSize(20)
+                                    .textWrap('none')
+                                    .textAlign('center')
+                                    .textVerticalAlign('center')
+                                    .margin(0);
+                            });
+
+
+                            //add verse text
+                            const font = getFont(line);
+                            slide.addText(text => {
+                                text.value(line)
+                                    .x(0)
+                                    .y(10)
+                                    .cx(720)
+                                    .cy(400)
+                                    .autoFit(true)
+                                    .fontFace('Calibri')
+                                    .fontSize(font)
+                                    // .textWrap('none')
+                                    .textAlign('center')
+                                    .textVerticalAlign('center')
+                                    .fontBold(true)
+                                    .margin(0);
+                            });
+                        });
+                    });
+                }
+            });
+        });
+    });
+}
+
+
+exports.run = async (req, res) => {
+    let pptx = new PPTX.Composer();
+    const { openingHymn, closingHymn } = req.query;
+
+    await pptx.compose(pres => {
+
+        pres.title('KHC Sabbath Program')
+            .author('Martin Kululanga')
+            .company('Kabula Hill SDA')
+            .revision('1')
+            .subject('KHC Sabbath Program')
+            .layout('LAYOUT_16x9');
+
+        // printSong('Court Worship', pres, 694, true);
+        printSong('Opening Hymn', pres, openingHymn);
+        printSong('Closing Hymn', pres, closingHymn);
+
+    });
+
+    await pptx.save(`./hymnal-songs.pptx`);
+    return res.status(200).json({ re: true });
+}
